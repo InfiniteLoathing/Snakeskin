144c124b63a6f7246ffb60379adbae572270ad46
InfiniteLoathing <s.sanderson@infiniteloathing.com>
2026-07-30 19:46:47 -0600

    Initial test classes

---
+++ Snakeskin/Snakeskin/Generator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace InfiniteLoathing.Snakeskin
+{
+    [Generator]
+    public class Generator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(AddEmbeddedAttribute);
+        }
+
+        private static void AddEmbeddedAttribute(IncrementalGeneratorPostInitializationContext context) =>
+            context.AddEmbeddedAttributeDefinition();
+    }
+}
